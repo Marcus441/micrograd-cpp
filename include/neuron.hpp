098dfbb5a6f62cc1dfb16bc0d48fd6d5e6013560
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-05-16 12:28:15 +1000

    feat(neuron): expose parameters api for neuron

--- include/neuron.hpp
+++ include/neuron.hpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <random>
 #include <vector>
 
@@ -8,9 +9,12 @@ class Neuron {
   Neuron(int n_inputs);
   auto operator()(const std::vector<Value>& x) const -> Value;
 
+  auto Parameters() -> std::shared_ptr<std::vector<Value>> { return parameters_; }
+
  private:
   static std::mt19937 rng;
   static std::uniform_real_distribution<double> dist;
+  std::shared_ptr<std::vector<Value>> parameters_;
   std::vector<Value> w_;
   Value b_;
 };
--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,4 +1,4 @@
-#include <neuron.hpp>
+#include "neuron.hpp"
 
 #include <cassert>
 
@@ -8,18 +8,28 @@ std::mt19937 Neuron::rng(std::random_device{}());
 std::uniform_real_distribution<double> Neuron::dist(-1.0, 1.0);
 
 Neuron::Neuron(int n_inputs) {
-  b_ = dist(rng);
+  parameters_ = std::make_shared<std::vector<Value>>();
+  parameters_->reserve(n_inputs + 1);
+
+  parameters_->emplace_back(dist(rng));
+  b_ = parameters_->at(0);
+
   for (int i = 0; i < n_inputs; ++i) {
-    w_.emplace_back(dist(rng));
+    parameters_->emplace_back(dist(rng));
+    w_.push_back(parameters_->back());
   }
-};
+}
 
 auto Neuron::operator()(const std::vector<Value>& x) const -> Value {
-  assert(x.size() == w_.size());
-  Value weight_sum;
-  for (size_t i = 0; i < w_.size(); ++i) {
-    weight_sum = weight_sum + (x[i] * w_[i]);
+  assert(x.size() == parameters_->size() - 1);
+
+  // Initialize with the bias
+  Value weight_sum = (*parameters_)[0];
+
+  for (size_t i = 0; i < x.size(); ++i) {
+    // Offset by 1 to skip the bias in the parameters vector
+    weight_sum = weight_sum + (x[i] * (*parameters_)[i + 1]);
   }
-  Value activation = (weight_sum + b_).Tanh();
-  return activation;
+
+  return weight_sum.Tanh();
 }
